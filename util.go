25bbf011f277b8c8c17c88b9de4929bf6caae2da
Stephan Peijnik <speijnik@anexia-it.com>
2017-05-23 12:25:50 +0200

    Add ForceStringMapKeys utility function
    This function ensures that all (nested) maps use string as the type of their
    keys. This allows structmapper with nested maps to be used by encoders
    such as the standard library's json encoder, which requires map keys to
    be strings.
    
    Signed-off-by: Stephan Peijnik <speijnik@anexia-it.com>

--- util.go
+++ util.go
@@ -1,6 +1,9 @@
 package structmapper
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 // This file contains utility functions
 
@@ -16,4 +19,157 @@ func IsNilOrEmpty(i interface{}, v reflect.Value) bool {
 
 	// Hard case: check if interface has "zero" value (ie. empty string, zero integer, etc.)
 	return reflect.DeepEqual(i, reflect.Zero(v.Type()).Interface())
+}
+
+// ForceStringMapKeys takes a map[string]interface{} and ensures that all maps which are nested
+// in this top-level map are of type map[string]interface{}.
+// The map that is passed in is expected to contain only primitive types, maps, slices and arrays.
+//
+// The purpose of this function is preparing a map returned by ToMap() to something encodings
+// like the standard library's JSON encoding can work with.
+//
+// Keys which are not strings already are converted to strings by either using the key's String() method,
+// if available, converting via reflect conversion or falling back to using fmt.Sprint() for conversion.
+func ForceStringMapKeys(in map[string]interface{}) (out map[string]interface{}, err error) {
+	out = make(map[string]interface{}, len(in))
+	for key, value := range in {
+		if out[key], err = convertToStringKeys(value); err != nil {
+			out = nil
+			return
+		}
+	}
+
+	return
+}
+
+func convertToStringKeys(in interface{}) (out interface{}, err error) {
+	return convertValueToStringKeys(reflect.ValueOf(in))
+}
+
+func convertValueToStringKeys(in reflect.Value) (out interface{}, err error) {
+	if !in.IsValid() || in.Interface() == nil {
+		return nil, nil
+	}
+
+	inKind := in.Kind()
+	switch inKind {
+	case reflect.Map:
+		out, err = convertMapToStringKeys(in)
+		return
+	case reflect.Slice:
+		out, err = convertSliceToStringKeys(in)
+		return
+	case reflect.Array:
+		out, err = convertArrayToStringKeys(in)
+		return
+	case reflect.Interface:
+		out, err = convertValueToStringKeys(in.Elem())
+		return
+	case reflect.Struct, reflect.Chan, reflect.Func:
+		err = fmt.Errorf("Conversion of type %s (kind: %s) is not supported", in.Type().String(),
+			inKind.String())
+	case reflect.Ptr:
+		if !in.IsNil() {
+			out, err = convertValueToStringKeys(in.Elem())
+			return
+		}
+
+		// If we received a nil-pointer we fall through to the default case
+	}
+
+	// Default: Any case not handled above is a primitive type which does not require conversion
+	out = in.Interface()
+	return
+}
+
+func convertSliceToStringKeys(in reflect.Value) (out interface{}, err error) {
+	// No-op for empty slices
+	if in.Len() == 0 {
+		return in.Interface(), nil
+	}
+
+	outSlice := reflect.MakeSlice(in.Type(), in.Len(), in.Cap())
+
+	for i := 0; i < in.Len(); i++ {
+		var val interface{}
+		if val, err = convertValueToStringKeys(in.Index(i)); err != nil {
+			return
+		}
+
+		outSlice.Index(i).Set(reflect.ValueOf(val))
+	}
+
+	out = outSlice.Interface()
+	return
+}
+
+func convertArrayToStringKeys(in reflect.Value) (out interface{}, err error) {
+	// No-op for empty arrays
+	if in.Len() == 0 {
+		return in.Interface(), nil
+	}
+
+	outArray := reflect.New(in.Type()).Elem()
+
+	for i := 0; i < in.Len(); i++ {
+		var val interface{}
+		if val, err = convertValueToStringKeys(in.Index(i)); err != nil {
+			return
+		}
+
+		outArray.Index(i).Set(reflect.ValueOf(val))
+	}
+
+	out = outArray.Interface()
+	return
+}
+
+func convertMapToStringKeys(in reflect.Value) (out map[string]interface{}, err error) {
+	stringType := reflect.TypeOf("")
+	inKeys := in.MapKeys()
+	out = make(map[string]interface{}, len(inKeys))
+	for _, key := range inKeys {
+		var keyString string
+		wasConverted := false
+		keyInterface := key.Interface()
+
+		if stringer, ok := keyInterface.(fmt.Stringer); ok {
+			// Key implements fmt.Stringer: use value returned by String()
+			keyString = stringer.String()
+			wasConverted = true
+		} else if goStringer, ok := keyInterface.(fmt.GoStringer); ok {
+			// Key implements fmt.GoStringer: use value returned by GoString()
+			keyString = goStringer.GoString()
+			wasConverted = true
+		} else if key.Kind() == reflect.String {
+			// Key is already a string or a type based on string: use key.String() to obtain the string
+			// value
+			keyString = key.String()
+			wasConverted = true
+		} else if key.Kind() == reflect.Interface && key.IsValid() &&
+			key.Elem().Type().ConvertibleTo(stringType) {
+			// Key is an interface, but has a type that is convertible to string underneath
+			switch key.Elem().Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint,
+				reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+				// No-op, as conversion from integer-types to string is possible using reflect,
+				// but gives us the unicode character corresponding to the integer's value
+			default:
+				keyString = key.Elem().Convert(stringType).Interface().(string)
+				wasConverted = true
+			}
+		}
+
+		if !wasConverted {
+			// Last resort: use fmt.Sprint to obtain a value
+			keyString = fmt.Sprint(keyInterface)
+		}
+
+		if out[keyString], err = convertValueToStringKeys(in.MapIndex(key)); err != nil {
+			out = nil
+			return
+		}
+	}
+
+	return
 }
--- util_test.go
+++ util_test.go
@@ -4,6 +4,8 @@ import (
 	"reflect"
 	"testing"
 
+	"time"
+
 	"github.com/anexia-it/go-structmapper"
 	"github.com/stretchr/testify/require"
 )
@@ -27,3 +29,429 @@ func TestIsNilOrEmpty(t *testing.T) {
 
 	// TODO: add additional test cases for types other than string
 }
+
+func TestForceStringMapKeys(t *testing.T) {
+	// Simple map test: should be returned as-is
+	simpleMap := map[string]interface{}{
+		"0":  "0",
+		"1":  int(1),
+		"2":  uint(2),
+		"3":  int8(3),
+		"4":  int16(4),
+		"5":  int32(5),
+		"6":  int64(6),
+		"7":  uint8(7),
+		"8":  uint16(8),
+		"9":  uint32(9),
+		"10": uint64(10),
+		"11": float32(11.1),
+		"12": float64(12.2),
+		"13": []byte{1, 3},
+		"14": complex(1.0, 4.0),
+		"15": true,
+		"16": nil,
+		"17": (*string)(nil),
+	}
+
+	res, err := structmapper.ForceStringMapKeys(simpleMap)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(simpleMap))
+	require.EqualValues(t, simpleMap, res)
+
+	// Simple map test: zero values
+	simpleMap = map[string]interface{}{
+		"0":  "",
+		"1":  int(0),
+		"2":  uint(0),
+		"3":  int8(0),
+		"4":  int16(0),
+		"5":  int32(0),
+		"6":  int64(0),
+		"7":  uint8(0),
+		"8":  uint16(0),
+		"9":  uint32(0),
+		"10": uint64(0),
+		"11": float32(0.0),
+		"12": float64(0.0),
+		"13": []byte{},
+		"14": complex(0.0, 0.0),
+		"15": false,
+		"16": nil,
+		"17": (*string)(nil),
+	}
+	res, err = structmapper.ForceStringMapKeys(simpleMap)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(simpleMap))
+	require.EqualValues(t, simpleMap, res)
+
+	// Test nesting with nested map[string]interface{}
+	nestedMapStringInterface := map[string]interface{}{
+		"0": map[string]interface{}{
+			"0":  "0",
+			"1":  int(1),
+			"2":  uint(2),
+			"3":  int8(3),
+			"4":  int16(4),
+			"5":  int32(5),
+			"6":  int64(6),
+			"7":  uint8(7),
+			"8":  uint16(8),
+			"9":  uint32(9),
+			"10": uint64(10),
+			"11": float32(11.1),
+			"12": float64(12.2),
+			"13": []byte{1, 3},
+			"14": complex(1.0, 4.0),
+			"15": true,
+			"16": nil,
+			"17": (*string)(nil),
+		},
+		"1": map[string]interface{}{
+			"0":  "0",
+			"1":  int(1),
+			"2":  uint(2),
+			"3":  int8(3),
+			"4":  int16(4),
+			"5":  int32(5),
+			"6":  int64(6),
+			"7":  uint8(7),
+			"8":  uint16(8),
+			"9":  uint32(9),
+			"10": uint64(10),
+			"11": float32(11.1),
+			"12": float64(12.2),
+			"13": []byte{1, 3},
+			"14": complex(1.0, 4.0),
+			"15": true,
+			"16": nil,
+			"17": (*string)(nil),
+		},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(nestedMapStringInterface)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(nestedMapStringInterface))
+	require.EqualValues(t, nestedMapStringInterface, res)
+
+	// Test nesting with slices and arrays
+	nestedSlicesArrays := map[string]interface{}{
+		"0": []string{"0.0", "0.1", "0.2"},
+		"1": []int{0, 1, 2},
+		"2": [3]uint{0, 1, 2},
+		"4": []interface{}{
+			map[string]interface{}{
+				"0":  "0",
+				"1":  int(1),
+				"2":  uint(2),
+				"3":  int8(3),
+				"4":  int16(4),
+				"5":  int32(5),
+				"6":  int64(6),
+				"7":  uint8(7),
+				"8":  uint16(8),
+				"9":  uint32(9),
+				"10": uint64(10),
+				"11": float32(11.1),
+				"12": float64(12.2),
+				"13": []byte{1, 3},
+				"14": complex(1.0, 4.0),
+				"15": true,
+				"16": nil,
+				"17": (*string)(nil),
+			},
+		},
+		"5": [2]interface{}{
+			map[string]interface{}{
+				"0":  "0",
+				"1":  int(1),
+				"2":  uint(2),
+				"3":  int8(3),
+				"4":  int16(4),
+				"5":  int32(5),
+				"6":  int64(6),
+				"7":  uint8(7),
+				"8":  uint16(8),
+				"9":  uint32(9),
+				"10": uint64(10),
+				"11": float32(11.1),
+				"12": float64(12.2),
+				"13": []byte{1, 3},
+				"14": complex(1.0, 4.0),
+				"15": true,
+				"16": nil,
+				"17": (*string)(nil),
+			},
+			map[string]interface{}{
+				"0":  "0",
+				"1":  int(1),
+				"2":  uint(2),
+				"3":  int8(3),
+				"4":  int16(4),
+				"5":  int32(5),
+				"6":  int64(6),
+				"7":  uint8(7),
+				"8":  uint16(8),
+				"9":  uint32(9),
+				"10": uint64(10),
+				"11": float32(11.1),
+				"12": float64(12.2),
+				"13": []byte{1, 3},
+				"14": complex(1.0, 4.0),
+				"15": true,
+				"16": nil,
+				"17": (*string)(nil),
+			},
+		},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(nestedSlicesArrays)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(nestedSlicesArrays))
+	require.EqualValues(t, nestedSlicesArrays, res)
+
+	// Test nesting with empty arrays and slices
+	nestedEmptyArraysSlices := map[string]interface{}{
+		"0": [0]string{},
+		"1": []int{},
+		"2": "test",
+	}
+
+	res, err = structmapper.ForceStringMapKeys(nestedEmptyArraysSlices)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(nestedEmptyArraysSlices))
+	require.EqualValues(t, nestedEmptyArraysSlices, res)
+
+	// Test key conversion: Stringer
+	now := time.Now()
+	duration := time.Millisecond * 10
+	keyConversionStringer := map[string]interface{}{
+		"0": map[interface{}]interface{}{
+			now:      0,
+			duration: 1,
+		},
+		"1": map[interface{}]interface{}{
+			now:      2,
+			duration: 3,
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(keyConversionStringer)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(keyConversionStringer))
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			now.String():      0,
+			duration.String(): 1,
+		},
+		"1": map[string]interface{}{
+			now.String():      2,
+			duration.String(): 3,
+		},
+	}, res)
+
+	// Test key conversion: GoStringer
+	keyConversionGoStringer := map[string]interface{}{
+		"0": map[interface{}]interface{}{
+			goStringerString("0"): 0,
+			goStringerString("1"): 1,
+		},
+		"1": map[interface{}]interface{}{
+			goStringerString("2"): 2,
+			goStringerString("3"): 3,
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(keyConversionGoStringer)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(keyConversionGoStringer))
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			"gostringer-0": 0,
+			"gostringer-1": 1,
+		},
+		"1": map[string]interface{}{
+			"gostringer-2": 2,
+			"gostringer-3": 3,
+		},
+	}, res)
+
+	// Test key conversion: convertible to string
+	keyConversionConvertible := map[string]interface{}{
+		"0": map[interface{}]interface{}{
+			convertibleToString("00"): 0,
+			convertibleToString("01"): 1,
+		},
+		"1": map[interface{}]interface{}{
+			convertibleToString("10"): 2,
+			convertibleToString("11"): 3,
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(keyConversionConvertible)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(keyConversionConvertible))
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			"00": 0,
+			"01": 1,
+		},
+		"1": map[string]interface{}{
+			"10": 2,
+			"11": 3,
+		},
+	}, res)
+
+	// Test key conversion: convertible to string, map has key type based on string
+	keyConversionConvertible2 := map[string]interface{}{
+		"0": map[convertibleToString]interface{}{
+			convertibleToString("00"): 0,
+			convertibleToString("01"): 1,
+		},
+		"1": map[convertibleToString]interface{}{
+			convertibleToString("10"): 2,
+			convertibleToString("11"): 3,
+		},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(keyConversionConvertible2)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(keyConversionConvertible2))
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			"00": 0,
+			"01": 1,
+		},
+		"1": map[string]interface{}{
+			"10": 2,
+			"11": 3,
+		},
+	}, res)
+
+	// Test key conversion: last resort using fmt.Sprint
+	keyConversionLastResort := map[string]interface{}{
+		"0": map[interface{}]interface{}{
+			0.5:     0,
+			uint(1): 1,
+			true:    2,
+		},
+		"1": map[interface{}]interface{}{
+			0.5:       0,
+			uint64(1): 1,
+			true:      2,
+		},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(keyConversionLastResort)
+	require.NoError(t, err)
+	require.NotNil(t, res)
+	require.Len(t, res, len(keyConversionLastResort))
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			"0.5":  0,
+			"1":    1,
+			"true": 2,
+		},
+		"1": map[string]interface{}{
+			"0.5":  0,
+			"1":    1,
+			"true": 2,
+		},
+	}, res)
+
+	// Test: struct value should give us an error
+	structValueMap := map[string]interface{}{
+		"0": testStruct{},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(structValueMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type structmapper_test.testStruct (kind: struct) is not supported")
+
+	// Test: chan value should give us an error
+	ch := make(chan bool)
+	defer close(ch)
+	chanValueMap := map[string]interface{}{
+		"0": ch,
+	}
+
+	res, err = structmapper.ForceStringMapKeys(chanValueMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type chan bool (kind: chan) is not supported")
+
+	// Test: func value should give us an error
+	funcValueMap := map[string]interface{}{
+		"0": testFn,
+	}
+
+	res, err = structmapper.ForceStringMapKeys(funcValueMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type func() (kind: func) is not supported")
+
+	// Test: pointer conversion
+	testString := "test"
+	pointerConversionMap := map[string]interface{}{
+		"0": map[interface{}]interface{}{
+			"0": &testString,
+		},
+	}
+
+	res, err = structmapper.ForceStringMapKeys(pointerConversionMap)
+	require.NoError(t, err)
+	require.EqualValues(t, map[string]interface{}{
+		"0": map[string]interface{}{
+			"0": testString,
+		},
+	}, res)
+
+	// Test: slice element value conversion error
+	sliceElementStructMap := map[string]interface{}{
+		"0": []interface{}{
+			testStruct{},
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(sliceElementStructMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type structmapper_test.testStruct (kind: struct) is not supported")
+
+	// Test: array element value conversion error
+	arrayElementStructMap := map[string]interface{}{
+		"0": [1]interface{}{
+			testStruct{},
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(arrayElementStructMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type structmapper_test.testStruct (kind: struct) is not supported")
+
+	// Test: map element value conversion error
+	// Additionally use a pointer to testStruct to also have pointer conversion in this test
+	mapElementStructMap := map[string]interface{}{
+		"0": map[string]interface{}{
+			"0": &testStruct{},
+		},
+	}
+	res, err = structmapper.ForceStringMapKeys(mapElementStructMap)
+	require.Nil(t, res)
+	require.EqualError(t, err, "Conversion of type structmapper_test.testStruct (kind: struct) is not supported")
+}
+
+type testStruct struct {
+}
+
+func testFn() {
+
+}
+
+type goStringerString string
+
+func (s goStringerString) GoString() string {
+	return "gostringer-" + string(s)
+}
+
+type convertibleToString string
